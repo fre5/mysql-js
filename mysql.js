dcdaf9543eb25cd980be211d4a21d9ef4d8004e1
enjsof <51922991+enjsof@users.noreply.github.com>
2019-11-18 13:42:04 -0800

    added more sample queries

--- mysql.js
+++ mysql.js
@@ -18,11 +18,49 @@ con.connect(function(err){
 console.log("Connected to MySQL...");
 
 
-//sample query
+//sample queries
+/*
  var sql = 'SELECT * FROM table';
              con.query(sql, function (err, result) {
              if (err) throw err;
              console.log(result);
              });
-             
+	     
+ var create = 'CREATE TABLE table100';
+             con.query(create, function (err, result) {
+             if (err) throw err;
+             console.log(result);
+             });
+	     
+ var insert = 'INSERT INTO table (field1, field2, ...) VALUES (value1, value2, ...)';
+             con.query(insert, function (err, result) {
+             if (err) throw err;
+             console.log(result);
+             });
+	     
+ var delete = 'DELETE FROM table1 WHERE condition';
+             con.query(delete, function (err, result) {
+             if (err) throw err;
+             console.log(result);
+             });
+	     
+ var update = 'UPDATE table1 SET field1=new_value1 WHERE condition';
+             con.query(update, function (err, result) {
+             if (err) throw err;
+             console.log(result);
+             });
+var alter = 'ALTER TABLE table MODIFY field1 type1';
+             con.query(alter, function (err, result) {
+             if (err) throw err;
+             console.log(result);
+             });
+	     
+var drop = 'DROP TABLE table';
+             con.query(drop, function (err, result) {
+             if (err) throw err;
+             console.log(result);
+             });
+*/
+	
+	
 });
